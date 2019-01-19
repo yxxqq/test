ab6a3350dafc3f2630ea56225a3d2a06590dc37e
yxx <3165346340@qq.com>
2019-01-19 11:14:49 +0800

    发送到repo5

---
+++ git_test/src/com/itheima/git/firstClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+        System.out.println("hellogit");
+    }
+}
